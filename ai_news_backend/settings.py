1b7518d845870452be39165662f08a6b3d897825
Dhiru Singh <dhirusingh9271@gmail.com>
2025-10-09 19:58:27 +0530

    debug false

--- ai_news_backend/settings.py
+++ ai_news_backend/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s77c5e1jj0u2po8##gt7%w#&i)q&o3mmuys#6p^%cxbok=8+xz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
